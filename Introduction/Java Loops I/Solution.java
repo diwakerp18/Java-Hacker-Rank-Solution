656abc3548ccd737be1c47e34c4434f38d36b85c
Diwakar Pathak <diwakarpathak@ninjacart.com>
2021-07-22 19:36:21 +0530

    Initial commit

---
+++ Introduction/Java Loops I/Solution.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Solution{
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in); 
+		int n = scan.nextInt();
+
+		for(int i=1; i<=10; i++){
+			int result = n*i;
+			System.out.println(n + " * " + i + " = " + result);
+		}
+
+	}
+}
